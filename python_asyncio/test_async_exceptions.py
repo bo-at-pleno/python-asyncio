f5f6cc655d844b973d6f23efdb823fef4451915c
Bo Lu <bo@plenoinc.com>
2022-09-23 09:12:01 -0700

    task queue poc

---
+++ python_asyncio/test_async_exceptions.py
@@ -0,0 +1,11 @@
+import asyncio
+
+
+def set_global_exception_handler():
+    """Allow for exception handling in event loop."""
+    def handle_exception(loop, context):
+        import sys
+        sys.print_exception(context["exception"])
+        sys.exit()
+    loop = asyncio.get_event_loop()
+    loop.set_exception_handler(handle_exception)
--- python_asyncio/test_async_task_queue.py
+++ python_asyncio/test_async_task_queue.py
@@ -1,43 +1,65 @@
 import asyncio
 import random
 import time
+from typing import Coroutine
 
 
-def make_random_async_task():
-    random_delay = random.randint(1, 5)
+def make_random_async_task(id: int) -> Coroutine:
+    random_delay = random.randint(2, 8)
 
     async def async_task():
-        print("task start ({})".format(random_delay))
+        print("task start ({})".format(id))
         await asyncio.sleep(random_delay)
-        print("task end ({})".format(random_delay))
+        print("task complete!({})".format(id))
+        return "task {} result, random wait was {}".format(id, random_delay)
 
-async def main(queue, output_queue):
+    return async_task
+
+
+async def executor(input_queue, output_queue):
+    print("Starting executor")
     while True:
-        task = await queue.get()
-
+        task = await input_queue.get()
         if task is None:
+            print("Got none, shutting down executor")
+            output_queue.put_nowait(None)
             break
 
-        print("Consuming {}".format(task))
+        print("Executing {}".format(task))
         # execute async task
-        task_res = await task
+        task_res = await task()
         output_queue.put_nowait(task_res)
 
-async def result_consumer(queue):
+
+async def main(input_queue, output_queue):
+    print("In main, kicking off executor and consumer")
+    for i in range(5):
+        print("adding task {} to input queue...".format(i))
+        input_queue.put_nowait(make_random_async_task(i))
+
+    # poison pill
+    input_queue.put_nowait(None)
+
+    await asyncio.gather(result_consumer(output_queue),
+                         executor(input_queue, output_queue))
+
+
+async def result_consumer(output_queue: asyncio.Queue):
+    print("Starting Result consumer")
     while True:
-        task_res = await queue.get()
+        task_res = await output_queue.get()
 
         if task_res is None:
+            print("Got None, shutting down consumer")
             break
 
         print("result: {}".format(task_res))
 
 if __name__ == '__main__':
-    queue = asyncio.Queue()
-    for i in range(10):
-        queue.put_nowait(make_random_async_task())
+    loop = asyncio.get_event_loop()
+    # put stuff into queue ahead of time from a synchronous thread
+    input_queue = asyncio.Queue(loop=loop)
+    output_queue = asyncio.Queue(loop=loop)
 
-    queue.put_nowait(None)
-    output_queue = asyncio.Queue()
-
-    asyncio.run(main(queue, output_queue))
+    # input_queue.put_nowait(None)
+    loop.run_until_complete(main(input_queue, output_queue))
