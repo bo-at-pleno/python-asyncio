b619dc61f10e2e9ebc781f6dbedefa0a943d5d10
Bo Lu <bo@plenoinc.com>
2022-10-07 09:51:02 -0700

    async wrapper

--- python_asyncio/test_async_wrapper.py
+++ python_asyncio/test_async_wrapper.py
@@ -6,7 +6,9 @@ import time
 from functools import partial, wraps
 from typing import Callable
 
-logging.basicConfig()
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s  [%(levelname)s]\t(%(process)d/%(thread)d)\t%(message)s')
+
 
 def async_wrap(func: Callable) -> Callable:
     """Wrap a function to be called asynchronously.
@@ -27,10 +29,9 @@ def async_wrap(func: Callable) -> Callable:
 
 
 @async_wrap
-def slow_hello():
+def slow_hello(name: str):
     import threading
-    logging.info("wrapped hello:" + threading.current_thread().name +
-                 " ///" + str(threading.get_ident()) + "/// pid: " + str(os.getpid()))
+    logging.info("wrapped hello:")
     logging.info("slow hello")
     random_delay = random.randint(1, 5)
     time.sleep(random_delay)
@@ -39,15 +40,15 @@ def slow_hello():
 
 async def hello():
     import threading
-    logging.info("async hello: " + threading.current_thread().name +
-                 " ///" + str(threading.get_ident()) + "/// pid: " + str(os.getpid()))
+    logging.info("async hello")
     logging.info("Hello")
     await asyncio.sleep(1)
     logging.info("World")
 
 
 async def main():
-    await asyncio.gather(hello(), slow_hello(), slow_hello(), slow_hello())
+    print("In main:")
+    await asyncio.gather(hello(), slow_hello("Bo"), slow_hello("Tom"), slow_hello("Jason"))
 
 if __name__ == '__main__':
     asyncio.run(main())
